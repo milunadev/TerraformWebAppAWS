4441cc57fb95c82600bd391c0b7e136fef8ce614
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-10-15 22:18:37 -0500

    Cambiando key de EC2

--- modules/compute.tf
+++ modules/compute.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance_1" {
   # Regla de seguridad para permitir todo el tráfico interno desde la red por defecto
   ami = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
-  key_name = "KeyPEM"
+  key_name = "SSHVMs.pem"
   tags = {
     Name =  var.ec2_name
   }
