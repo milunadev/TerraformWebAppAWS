4764f95eb8fcc7f18f3a5dfba2878219887d298a
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-10-15 23:46:47 -0500

    Agregando 3001 inbound rule

--- modules/networking.tf
+++ modules/networking.tf
@@ -26,11 +26,19 @@ resource "aws_security_group_rule" "allow_frontend" {
   protocol = "tcp"
   cidr_blocks = [ "0.0.0.0/0" ]
 }
+resource "aws_security_group_rule" "allow_backe" {
+  type = "ingress"
+  security_group_id = aws_security_group.MilunaWEB_EC2_SG.id
+  from_port = 3001
+  to_port = 3001
+  protocol = "tcp"
+  cidr_blocks = [ "0.0.0.0/0" ]
+}
 resource "aws_security_group_rule" "allow_ssh" {
   type = "ingress"
   security_group_id = aws_security_group.MilunaWEB_EC2_SG.id
   from_port = 22
-  to_port = 22
+  to_port = 22  
   protocol = "tcp"
   cidr_blocks = [ "0.0.0.0/0" ]
 }
